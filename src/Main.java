799ee0971eb31873bc0d286c7dda74ba8f3e53e5
Andrey Igumnov <igumnov92@gmail.com>
2022-08-19 15:05:33 +0300

    Изменения в гитигнор

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         SalesManager sale = new SalesManager(new int[] {3, 4, 5, 1, 2});
-        System.out.println("Маскимальное количество сделок за неделю: " + sale.max());
+        System.out.println("Максимальное количество сделок за неделю: " + sale.max());
 
     }
 }
