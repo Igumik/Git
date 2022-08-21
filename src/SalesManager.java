27e4e61a43ccb835a3699e0b62798015d608c822
Andrey Igumnov <igumnov92@gmail.com>
2022-08-21 12:50:48 +0300

    Создание и реализация метода avg()

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -5,6 +5,24 @@ public class SalesManager {
         this.sales = sales;
     }
 
+    public int avg() {
+        int total = 0;
+        int max = -1;
+        int min = 100;
+        for (int sale : sales) {
+            if (sale > max) {
+                max = sale;
+            }
+            if (sale < min) {
+                min = sale;
+            }
+            if (sale > 0) {
+                total += sale;
+            }
+        }
+        return (total - max - min) / (sales.length - 2);
+    }
+
     public int max() {
         int max = -1;
         for (int sale : sales) {
