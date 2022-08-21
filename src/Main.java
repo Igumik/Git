cc9249df50861231a93a56ad0ee21b137afab1d0
Andrey Igumnov <igumnov92@gmail.com>
2022-08-21 11:42:08 +0300

    Изменение с int на long

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        SalesManager sale = new SalesManager(new int[] {3, 4, 5, 1, 2});
+        SalesManager sale = new SalesManager(new long[] {3, 4, 5, 1, 2});
         System.out.println("Максимальное количество сделок за неделю: " + sale.max());
 
     }
